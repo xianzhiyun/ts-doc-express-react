40ce40944d652719b34c69badde7e55da14113a1
zhangle <zhangle@vtstar.net>
2020-03-02 09:26:49 +0800

    feat(🎉初始化代码):

---
+++ .vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    base: '/vt-ui/',
+    dest: 'dist',
+    title: 'TypeScript 学习文档',
+    description: '爬虫、express + TypeScript',
+    markdown: { //markdown编辑器的配置
+        lineNumbers: true, //代码显示行号，默认 false
+        toc: {includeLevel: [1]}, //显示目录的默认层级
+    },
+    themeConfig: {
+        editLinks: false,
+        nav: [],
+        docsDir: 'packages',
+        sidebar: [
+            {
+                title: '指南',
+                collapsable: false,
+                children: [
+                    ['docs/', '简介'],
+                    'docs/start'
+                ]
+            },
+        ],
+        // 文档更新时间：每个文件git最后提交的时间,
+        lastUpdated: 'Last Updated',
+    }
+}
