19cd48c17b3018d849676dafd24cb653fed34f7e
zhangle <zhangle@vtstar.net>
2020-03-05 21:16:00 +0800

    feat(初始化代码): 普通代码的实现🚀

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,33 +1,70 @@
 import superagent from 'superagent'
 import cheerio from 'cheerio'
+import fs from 'fs'
+import path from 'path'
 
+interface Course {
+    title: string,
+    per: string
+}
+
+interface CourseResult {
+    time: number,
+    data: Course[]
+}
+
+interface Content {
+    [propName:number]:Course[]
+}
 class Crowller {
     private url = 'https://www.zhuwang.cc/';
     private rawHtml = '';
 
     async getRawHtml() {
-        try{
-            const result = await superagent.get(this.url).set({
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-            })
-            this.getCourseInfo(result.text)
-        }catch (e) {
-            console.log(e)
-        }
+        const result = await superagent.get(this.url).set({
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        });
+        return result.text
     }
-    getCourseInfo(html:string){
+
+    getCourseInfo(html: string) {
+        let courseInfos: Course[] = [];
         const $ = cheerio.load(html)
         const courseItems = $('.chart_nav>ul>li')
         console.log(courseItems.length)
-        // courseItems.map((index, element) => {
-        //     const descs = $(element).find('entry-list');
-        //     const title = descs.eq(0).text()
-        //     // console.log(title)
-        // })
+        courseItems.map((index, element) => {
+            const title = $(element).find('li>p').eq(0).text();
+            const per = $(element).find('li>div').eq(0).text();
+            courseInfos.push({
+                title,
+                per
+            })
+        })
+        return {
+            time: new Date().getTime(),
+            data: courseInfos
+        }
     }
 
-     constructor() {
-        this.getRawHtml()
+    generateJsonContent(courseInfo: CourseResult) {
+        let fileContent:Content = {}
+        const filePath = path.resolve(__dirname,'../data/course.json')
+        if (fs.existsSync(filePath)) {
+            fileContent = JSON.parse(fs.readFileSync(filePath,'utf-8'))
+        }
+        return  fileContent[courseInfo.time] = courseInfo.data
+    }
+
+    async initSpiderProcess() {
+        const filePath = path.resolve(__dirname,'../data/course.json')
+        const html = await this.getRawHtml()
+        const courseInfo = this.getCourseInfo(html);
+        const fileContent =  this.generateJsonContent(courseInfo)
+        fs.writeFileSync(filePath, JSON.stringify(fileContent))
+    }
+
+    constructor() {
+        this.initSpiderProcess()
     }
 }
 
